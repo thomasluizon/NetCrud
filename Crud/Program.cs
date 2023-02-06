b7410a5e8b42813a37e98c6e22be5dcbdcb8bd58
thomas-luizon <thomaslrgregorio@gmail.com>
2023-02-06 10:52:56 -0300

    first commit

---
+++ Crud/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.Run();
